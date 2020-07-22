b9e9caf8f7482566372aba564d2d70cb61681063
Jai Govindani <jai@honestbank.com>
2020-07-22 12:26:50 +0700

    feat(terraform): add remote-backend.tf
    Signed-off-by: Jai Govindani <jai@honestbank.com>

---
+++ remote-backend.tf
@@ -0,0 +1,10 @@
+# terraform {
+#   backend "remote" {
+#     hostname     = "app.terraform.io"
+#     organization = "honestbank"
+
+#     workspaces {
+#       name = "terraform-gcp-gke-template"
+#     }
+#   }
+# }
