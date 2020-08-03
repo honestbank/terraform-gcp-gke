45fd1b7aff62242f8e1d63f3b31240185c88d667
Jai Govindani <jai@honestbank.com>
2020-08-03 13:00:00 +0700

    fix(*): terraform cloud issues (#6)
    * fix(terraform): download kubectl
    
    Signed-off-by: Jai Govindani <jai@honestbank.com>
    
    * fix(terraform): download kubectl via gcloud
    
    Signed-off-by: Jai Govindani <jai@honestbank.com>

--- gcp-gke/main.tf
+++ gcp-gke/main.tf
@@ -187,14 +187,13 @@ data "google_container_cluster" "current_cluster" {
   location = module.primary-cluster.location
 }
 
-# configure kubectl with the credentials of the GKE cluster
-resource "null_resource" "configure_kubectl" {
+# set up the gcloud command line tools
+resource "null_resource" "setup_gcloud_cli" {
   provisioner "local-exec" {
     command = <<EOH
   curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-302.0.0-linux-x86_64.tar.gz | tar xz
   cat <<< '${var.google_credentials}' > google_credentials_keyfile.json
   ./google-cloud-sdk/bin/gcloud auth activate-service-account --key-file google_credentials_keyfile.json --quiet
-  ./google-cloud-sdk/bin/gcloud container clusters get-credentials "${module.primary-cluster.name}" --region "${var.region}" --project "${var.project}" --quiet
   EOH
     # Use environment variables to allow custom kubectl config paths
     //    environment = {
@@ -207,6 +206,27 @@ resource "null_resource" "configure_kubectl" {
   depends_on = [module.primary-cluster]
 }
 
+# download kubectl
+resource "null_resource" "download_kubectl" {
+  provisioner "local-exec" {
+    # command = "curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl"
+    command = "if ! command -v kubectl; then gcloud components install kubectl --quiet; fi;"
+  }
+
+  depends_on = [null_resource.setup_gcloud_cli]
+}
+
+# get kubeconfig
+resource "null_resource" "configure_kubectl" {
+  provisioner "local-exec" {
+    command = <<EOH
+      ./google-cloud-sdk/bin/gcloud container clusters get-credentials "${module.primary-cluster.name}" --region "${var.region}" --project "${var.project}" --quiet
+EOH
+  }
+
+  depends_on = [null_resource.download_kubectl]
+}
+
 # Install Istio Operator using istioctl
 resource "null_resource" "install_istio_operator" {
   provisioner "local-exec" {
@@ -214,7 +234,7 @@ resource "null_resource" "install_istio_operator" {
 curl -sL https://istio.io/downloadIstioctl | sh -
 export PATH=$PATH:$HOME/.istioctl/bin
 istioctl operator init
-kubectl label namespace default istio-injection=enabled
+./kubectl label namespace default istio-injection=enabled
 EOH
   }
 
@@ -225,12 +245,12 @@ EOH
 resource "null_resource" "set_kiali_credentials" {
   provisioner "local-exec" {
     command = <<EOH
-kubectl create ns istio-system
+./kubectl create ns istio-system
 KIALI_USERNAME=$(printf "${var.kiali_username}" | base64)
 echo "Kiali Username (base64): "$KIALI_USERNAME
 KIALI_PASSPHRASE=$(printf "${var.kiali_passphrase}" | base64)
 echo "Kiali Passphrase (base64): "$KIALI_PASSPHRASE
-cat <<EOF | kubectl apply -f -
+cat <<EOF | ./kubectl apply -f -
 apiVersion: v1
 kind: Secret
 metadata:
@@ -253,7 +273,7 @@ EOH
 resource "null_resource" "install_IstioOperator_manifest" {
   provisioner "local-exec" {
     command = <<EOH
-cat <<EOF | kubectl apply -f -
+cat <<EOF | ./kubectl apply -f -
 apiVersion: install.istio.io/v1alpha1
 kind: IstioOperator
 metadata:
@@ -277,7 +297,7 @@ EOH
 resource "null_resource" "install_Elastic_operator" {
   provisioner "local-exec" {
     command = <<EOH
-kubectl apply -f https://download.elastic.co/downloads/eck/1.2.0/all-in-one.yaml
+./kubectl apply -f https://download.elastic.co/downloads/eck/1.2.0/all-in-one.yaml
 EOH
   }
 
@@ -288,9 +308,9 @@ EOH
 resource "null_resource" "install_Elastic_resources" {
   provisioner "local-exec" {
     command     = <<EOH
-kubectl create -f 'modules/elastic/elastic-basic-cluster.yaml'
-kubectl create -f 'modules/elastic/elastic-filebeat.yaml'
-kubectl create -f 'modules/elastic/elastic-kibana.yaml'
+./kubectl create -f 'modules/elastic/elastic-basic-cluster.yaml'
+./kubectl create -f 'modules/elastic/elastic-filebeat.yaml'
+./kubectl create -f 'modules/elastic/elastic-kibana.yaml'
 EOH
     working_dir = path.module
   }
