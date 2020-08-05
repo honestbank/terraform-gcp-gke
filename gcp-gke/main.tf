07f8dbd6984bae9088ae3803c653db7d792d62e6
Jai Govindani <jai@honestbank.com>
2020-08-05 18:03:58 +0700

    chore(terraform/kubectl): change kubectl path
    Signed-off-by: Jai Govindani <jai@honestbank.com>

--- gcp-gke/main.tf
+++ gcp-gke/main.tf
@@ -239,7 +239,7 @@ resource "null_resource" "install_istio_operator" {
 curl -sL https://istio.io/downloadIstioctl | sh -
 export PATH=$PATH:$HOME/.istioctl/bin
 istioctl operator init
-kubectl label namespace default istio-injection=enabled
+./kubectl label namespace default istio-injection=enabled
 EOH
   }
 
@@ -250,12 +250,12 @@ EOH
 resource "null_resource" "set_kiali_credentials" {
   provisioner "local-exec" {
     command = <<EOH
-kubectl create ns istio-system
+./kubectl create ns istio-system
 KIALI_USERNAME=$(printf "${var.kiali_username}" | base64)
 echo "Kiali Username (base64): "$KIALI_USERNAME
 KIALI_PASSPHRASE=$(printf "${var.kiali_passphrase}" | base64)
 echo "Kiali Passphrase (base64): "$KIALI_PASSPHRASE
-cat <<EOF | kubectl apply -f -
+cat <<EOF | ./kubectl apply -f -
 apiVersion: v1
 kind: Secret
 metadata:
@@ -278,7 +278,7 @@ EOH
 resource "null_resource" "install_IstioOperator_manifest" {
   provisioner "local-exec" {
     command = <<EOH
-cat <<EOF | kubectl apply -f -
+cat <<EOF | ./kubectl apply -f -
 apiVersion: install.istio.io/v1alpha1
 kind: IstioOperator
 metadata:
@@ -302,7 +302,7 @@ EOH
 resource "null_resource" "install_Elastic_operator" {
   provisioner "local-exec" {
     command = <<EOH
-kubectl apply -f https://download.elastic.co/downloads/eck/1.2.0/all-in-one.yaml
+./ apply -f https://download.elastic.co/downloads/eck/1.2.0/all-in-one.yaml
 EOH
   }
 
@@ -313,9 +313,9 @@ EOH
 resource "null_resource" "install_Elastic_resources" {
   provisioner "local-exec" {
     command     = <<EOH
-kubectl create -f 'modules/elastic/elastic-basic-cluster.yaml'
-kubectl create -f 'modules/elastic/elastic-filebeat.yaml'
-kubectl create -f 'modules/elastic/elastic-kibana.yaml'
+./kubectl create -f 'modules/elastic/elastic-basic-cluster.yaml'
+./kubectl create -f 'modules/elastic/elastic-filebeat.yaml'
+./kubectl create -f 'modules/elastic/elastic-kibana.yaml'
 EOH
     working_dir = path.module
   }
