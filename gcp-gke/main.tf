dd70323dd1a21523382a7fd4fb71ef13c161cfba
Jai Govindani <jai@honestbank.com>
2020-08-06 17:31:08 +0700

    fix(download_kubectl): change shell command
    Signed-off-by: Jai Govindani <jai@honestbank.com>

--- gcp-gke/main.tf
+++ gcp-gke/main.tf
@@ -223,8 +223,7 @@ resource "null_resource" "download_kubectl" {
   }
   provisioner "local-exec" {
     command = <<EOH
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
 export PATH=$PATH:$(pwd)
 EOH
   }
