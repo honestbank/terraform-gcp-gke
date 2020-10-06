2c0bea0d598c95c547df921abc89863760b2225d
Jai Govindani <jai@honestbank.com>
2020-10-06 12:13:48 +0700

    style(remote-backend): terraform fmt
    Signed-off-by: Jai Govindani <jai@honestbank.com>

--- remote-backend.tf
+++ remote-backend.tf
@@ -14,12 +14,12 @@ terraform {
 module gke {
   source = "./gcp-gke"
 
-  google_project     = var.google_project
-  google_region      = var.google_region
-  google_credentials = var.google_credentials
-  shared_vpc_host_google_project = var.shared_vpc_host_google_project
+  google_project                     = var.google_project
+  google_region                      = var.google_region
+  google_credentials                 = var.google_credentials
+  shared_vpc_host_google_project     = var.shared_vpc_host_google_project
   shared_vpc_host_google_credentials = var.shared_vpc_host_google_credentials
-  stage              = var.stage
-  cluster_purpose    = var.cluster_purpose
-  cluster_number     = var.cluster_number
+  stage                              = var.stage
+  cluster_purpose                    = var.cluster_purpose
+  cluster_number                     = var.cluster_number
 }
