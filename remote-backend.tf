07b23cc9a5729c975c4d1846fc0352016803b298
Jai Govindani <jai@honestbank.com>
2020-10-12 15:47:40 +0700

    fix(*): elasticsearch resource fields, GKE node pool size (#31)
    * fix(elasticsearch): add resource requests/limits
    
    Signed-off-by: Jai Govindani <jai@honestbank.com>
    
    * fix(gke): reduce default node pool size to 1
    
    Signed-off-by: Jai Govindani <jai@honestbank.com>
    
    * fix(gcp-gke/variables): missing trailing quote
    
    Signed-off-by: Jai Govindani <jai@honestbank.com>
    
    * style(remote-backend): terraform fmt
    
    Signed-off-by: Jai Govindani <jai@honestbank.com>
    
    * fix(gke): propagate vars into module
    
    Signed-off-by: Jai Govindani <jai@honestbank.com>

--- remote-backend.tf
+++ remote-backend.tf
@@ -17,9 +17,19 @@ module gke {
   google_project     = var.google_project
   google_region      = var.google_region
   google_credentials = var.google_credentials
+  
   shared_vpc_host_google_project = var.shared_vpc_host_google_project
   shared_vpc_host_google_credentials = var.shared_vpc_host_google_credentials
+  
   stage              = var.stage
   cluster_purpose    = var.cluster_purpose
   cluster_number     = var.cluster_number
+  
+  machine_type = var.machine_type
+  minimum_node_count = var.minimum_node_count
+  maximum_node_count = var.maximum_node_count
+  initial_node_count = var.initial_node_count
+
+  kiali_username = var.kiali_username
+  kiali_passphrase = var.kiali_passphrase
 }
