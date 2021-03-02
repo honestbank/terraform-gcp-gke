76390d74d2afb2a7ea19b235a19418ce99c21c9b
Jai Govindani <jai@honestbank.com>
2021-03-02 13:28:16 +0700

    refactor(outputs): terraform fmt
    Signed-off-by: Jai Govindani <jai@honestbank.com>

--- outputs.tf
+++ outputs.tf
@@ -26,7 +26,7 @@ output "client_token" {
 
 output "ca_certificate" {
   sensitive = true
-  value = module.gke.ca_certificate
+  value     = module.gke.ca_certificate
 }
 
 output "service_account" {
